217d1e8140ae77bd04a28726c18f177db34e613e
Emiton Alves <emiton_alves@my.uri.edu>
2019-04-25 12:41:10 -0400

    Completed ROC analysis for GDP and budget

--- spending_analysis.R
+++ spending_analysis.R
@@ -1,8 +1,10 @@
 # Packages
 install.packages("tidyverse")
+install.packages("reshape2")
 
 # Libraries
 library("tidyverse")
+library("reshape2")
 
 # Import data
 climate_spend <- read.csv("data/climate_spending.csv")
@@ -43,19 +45,46 @@ yearly_outlays <- fed_spend %>%
   select(year, total_outlays)
 
 # discretionary_fed_spend
-yearly_discretionary_spending <- fed_spend %>%
+yearly_disc_spend <- fed_spend %>%
   filter(department == "DOD") %>%
   select(year, discretionary_outlays)
 
-# rate of change for yearly gdp
-temp_rate <- 100*diff(yearly_gdp$gdp)/yearly_gdp[-nrow(yearly_gdp),]$gdp
-length(temp_rate)
 
-# 1. gdp roc with total_spend and disc_spend [R2]
 
-# 2. gdp roc vs per group roc (fed, energy, climate) [R2]
+# A1 - Method 1: gdp roc with total_spend and disc_spend [R2]
 
-# 3. gdp roc versus every department [R2]
+## rate of change for yearly gdp
+gdp_change <- 100*diff(yearly_gdp$gdp)/yearly_gdp[-nrow(yearly_gdp),]$gdp
+total_outlay_change <- 100*diff(yearly_outlays$total_outlays)/yearly_outlays[-nrow(yearly_outlays),]$total_outlays
+disc_outlay_change <- 100*diff(yearly_disc_spend$discretionary_outlays)/yearly_disc_spend[-nrow(yearly_discretionary_spending),]$discretionary_outlays
+
+gdp_budget_roc <- data.frame("Year" = yearly_gdp$year[2:42], 
+                             "gdp_change" = gdp_change,
+                             "outlay_change" = total_outlay_change,
+                             "disc_change" = disc_outlay_change)
+
+p <- ggplot() +
+  geom_line(data = gdp_budget_roc, aes(x = Year, y = gdp_change), color = "blue") +
+  geom_line(data = gdp_budget_roc, aes(x = Year, y = outlay_change), color = "red") + 
+  geom_line(data = gdp_budget_roc, aes(x = Year, y = disc_change), color = "green") +
+  xlab("Year") + 
+  ylab("Rate of change (%)")
+
+print(p)
+
+
+# A1 - Method 2: Melt data and then plot
+roc_melt <- melt(gdp_budget_roc, id=c("Year"))
+
+ggplot(roc_melt) +
+  geom_line(aes(x = Year, y = value, color=variable)) +
+  scale_color_manual(values = c("red","green", "blue"),
+                     labels = c("GDP", "Total Spending", "Discretionary Spending"))
+
+
+# A2. gdp roc vs per group roc (fed, energy, climate) [R2]
+
+# A3. gdp roc versus every department [R2]
 
 # 1,2 3 repeated with total spending in place of gdp
 # 1,2,3 repeated with disc spending in place of gdp
